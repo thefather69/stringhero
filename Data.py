1b4824c200bf46d62ae945d99d32da7b4bc5bb3f
thefather69 <110205195+thefather69@users.noreply.github.com>
2022-08-14 07:59:08 +0530

    Initial commit

---
+++ Data.py
@@ -0,0 +1,57 @@
+from pyrogram.types import InlineKeyboardButton
+
+
+class Data:
+    # Start Message
+    START = """
+Hey {}
+
+Welcome to {}
+
+You can use me to generate pyrogram and telethon string session. Use the below buttons to know more!
+    """
+
+    # Home Button
+    home_buttons = [
+        [InlineKeyboardButton("⚜ Start Generating Session ⚜", callback_data="generate")],
+        [InlineKeyboardButton(text=" Back ", callback_data="home")]
+    ]
+
+    generate_button = [
+        [InlineKeyboardButton("🔥 Start Generating Session 🔥", callback_data="generate")]
+    ]
+
+    support_button = [
+        [InlineKeyboardButton("⚜ Support ⚜", url="https://t.me/tigernetwork"")]
+    ]
+
+    # Rest Buttons
+    buttons = [
+        [InlineKeyboardButton("🔥 Start Generating Session 🔥", callback_data="generate")],
+        [InlineKeyboardButton("👨‍💻 Network 👨‍💻", url="https://t.me/tigernetwork")],
+        [
+            InlineKeyboardButton("How to Use ❔", callback_data="help"),
+            InlineKeyboardButton(" About", callback_data="about")
+        ],
+        [InlineKeyboardButton("🇮🇳 Owner 🇮🇳", url="https://t.me/tigernetwork")],
+    ]
+
+    # Help Message
+    HELP = """
+» Click the below button or use /generate command to start generating session!
+» Click the required button; [Pyrogram/Telethon]
+» Enter the required variables when asked.
+"""
+
+    # About Message
+    ABOUT = """
+👨‍💻 **About Me** 
+
+A telegram bot to generate pyrogram and telethon string session...
+
+[Pyrogram](docs.pyrogram.org)
+[Telethon](docs.telethon.org)
+
+Language : [Python](www.python.org)
+            **Regarding ~ **@thefather69
+"""
---
+++ README.md
@@ -0,0 +1,16 @@
+# STRING-GENERATOR BOT
+
+- This Bot Can Help U To Generate String Session Pyrogram and Telethon 
+
+- ⚠️ Note : Dont Try To Missuse This Bot.
+
+------------
+<h3> Dєρℓογ το нєяοκυ </h3>
+
+- FORK THIS REPO
+
+- THEN CLICK BELOW BUTTON 
+
+- [![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
+
+------------
